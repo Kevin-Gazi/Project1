11b81f73d1b2b227572d8fd9b2cdd61132d9ca51
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-04-04 13:26:02 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-
+import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
         System.out.println("Hello world!");
 
-        // test
+        QuestionReader reader = new QuestionReader("<Locatie uit te lezen bestand>");
+        List<String> vragen = reader.readAllLines();
+
+        AnswerWriter writer = new AnswerWriter("<Locatie uit te lezen bestand>");
+        writer.writeAllLines(List.of("42", "Extra test"));
+
+        for (String vraag : vragen) {
+            System.out.println(vraag);
+        }
+
     }
 }
