1a4f80367a50f9822f1dda756406f473f8b1e333
adamz <adam.zuurveld@gmail.com>
2024-04-04 13:14:07 +0200

    Klassen toegevoegd

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         System.out.println("Hello world!");
         String goenoes = "apen";
-        String goenoes1 = "apen";
-        String goenoes2= "apen";
-        String goenoes3 = "apen";
-        String goenoes4 = "apen";
-        String goenoes5 = "apen";
     }
 }
