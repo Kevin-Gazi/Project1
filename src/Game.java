ccbef000d802b4c2004711d32d69cf519fa31133
kurty <kurtyek17@gmail.com>
2024-04-04 14:31:51 +0200

    Class-Game en Vraag

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class Game {
-    String gameNaam;
-    String gameGenre;
-    double gamePrijs;
-    ArrayList<Review> gameLijst;
+    private String gameNaam;
+    private String gameGenre;
+    private double gamePrijs;
+    private ArrayList<Review> gameLijst;
 
     public void voegGameToe() {
 
