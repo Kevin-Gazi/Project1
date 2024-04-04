14058ea280b075c190c0ee1cd33984faea656eb2
kurty <kurtyek17@gmail.com>
2024-04-04 13:43:30 +0200

    Class-Review

--- src/Review.java
+++ src/Review.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
 public class Review {
     double gameplayScore;
     double storyLineScore;
-
+    double graphicsScore;
+    String toelichting;
+    ArrayList<Vraag> review;
 }
+
+ public double berekenGemiddelde() {
+
+ }
+
