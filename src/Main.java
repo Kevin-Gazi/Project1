8ec65e2436cff7202d4a40a8c24ad55787884d1e
faris <farishalepoto@gmail.com>
2024-04-04 14:38:25 +0200

    menukeuze

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String fileName = "mydata.csv";
-        AnswerWriter.writeToCSV(fileName); // Roep de methode aan om naar CSV te schrijven
+        MenuKeuze menu = new MenuKeuze();
+        menu.start();
     }
 }
