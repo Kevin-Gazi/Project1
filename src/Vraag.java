1dee9d05ccf50c5ebf73356dcc651974b5248485
kurty <kurtyek17@gmail.com>
2024-04-05 09:57:22 +0200

    Class-Vraag

--- src/Vraag.java
+++ src/Vraag.java
@@ -2,9 +2,19 @@ import java.util.ArrayList;
 
 public class Vraag {
     private String vraag;
-    private ArrayList<String> vervolgVraag;
+    private ArrayList<String> vervolgVraag = new ArrayList<>();
 
+    public Vraag (String vraag){
+        this.vraag = vraag;
+    }
+    public String getVraag() {
+        return vraag;
+    }
+    public void setVraag (String vraag) {
+        this.vraag = vraag;
+    }
     public void voegVraagToe() {
+        vervolgVraag.add(vraag);
 
     }
 
