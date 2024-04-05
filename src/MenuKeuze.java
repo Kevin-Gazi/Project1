5cee31882004e5bdc02755d846f162ed1c2f0fa3
faris <farishalepoto@gmail.com>
2024-04-05 09:38:52 +0200

    voeggametoe methode in game gestopt error exception voor voeggametoe

--- src/MenuKeuze.java
+++ src/MenuKeuze.java
@@ -11,7 +11,7 @@ public class MenuKeuze {
         System.out.println("=== Retro Game Store ===");
         System.out.println("1. Toon alle games");
         System.out.println("2. Voeg nieuwe game toe");
-        System.out.println("3. Toon reviews op basis van genre");
+        System.out.println("3. Toon games op basis van genre");
         System.out.println("4. Voeg een review toe voor een game");
         System.out.println();
         System.out.println("5. Afsluiten");
@@ -25,14 +25,14 @@ public class MenuKeuze {
             displayMainMenu();
             System.out.print("Voer uw keuze in: ");
             choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline character
+            scanner.nextLine();
 
             switch (choice) {
                 case 1:
                     catalogus.toonCatalogus();
                     break;
                 case 2:
-                    voegGameToe(scanner);
+                    Game.voegGameToe(scanner, catalogus);
                     break;
                 case 3:
                     System.out.print("Voer het genre in om de reviews te tonen: ");
@@ -47,17 +47,5 @@ public class MenuKeuze {
                     break;
             }
         } while (choice != 5);
-    }
-
-    public void voegGameToe(Scanner scanner) {
-        System.out.print("Voer de titel van de nieuwe game in: ");
-        String titel = scanner.nextLine();
-        System.out.print("Voer het genre van de nieuwe game in: ");
-        String genre = scanner.nextLine();
-        System.out.print("Voer de prijs van de nieuwe game in: ");
-        double prijs = scanner.nextDouble();
-        Game nieuweGame = new Game(titel, genre, prijs);
-        catalogus.voegGameToe(nieuweGame);
-        System.out.println("Nieuwe game toegevoegd aan de catalogus!");
     }
 }
