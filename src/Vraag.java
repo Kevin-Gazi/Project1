948e36a83907cade8f7c8df9b0662c4a38a5ab11
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-04-06 19:33:42 +0200

    class vraag

--- src/Vraag.java
+++ src/Vraag.java
@@ -6,26 +6,24 @@ public class Vraag {
 
     Scanner scanner = new Scanner(System.in);
 
-    String[] openVragen = {
+    private String[] openVragen = {
             "wat is uw leeftijd?",
             "Welke game trekt u van onze webshop het meest aan?",
             "Zijn er andere soorten games die u in ons voorraad zou willen zien?"};
 
-    String[] openVragenMetVervolg = {
+    private String[] openVragenMetVervolg = {
             "Zou je onze website aanbevelen aan andere voor het achterlaten van reviews? (ja/nee)",
             "Heb je ooit moeilijkheden ondervonden bij het vinden van een specifiek spel in de catalogus? (ja/nee)"};
 
-    // Meerkeuze vragen
-    String[] meerkeuzeVragen = {
+    private String[] meerkeuzeVragen = {
             "Welke soort games spreekt u het meest aan? (meerdere keuzes mogelijk)",
             "Hoe tevreden ben je over de gebruiksvriendelijkheid voor het achterlaten van reviews? (meerdere keuzes mogelijk)"};
-    String[][] meerkeuzeOpties = {
+    private String[][] meerkeuzeAntwoorden = {
             {"Actie", "Avontuur", "Horror", "Strategie"},
             {"Zeer tevreden", "Tevreden", "Neutraal", "Ontevreden", "Zeer ontevreden"}
     };
 
-    List<String> antwoorden = new ArrayList<>();
-
+    private List<String> antwoorden = new ArrayList<>();
 
     public void VragenBeantwoorden() {
 
@@ -49,10 +47,10 @@ public class Vraag {
 
         for (int i = 0; i < meerkeuzeVragen.length; i++) {
             System.out.println(meerkeuzeVragen[i]);
-            for (int j = 0; j < meerkeuzeOpties[i].length; j++) {
-                System.out.println((char) ('a' + j) + ") " + meerkeuzeOpties[i][j]);
+            for (int j = 0; j < meerkeuzeAntwoorden[i].length; j++) {
+                System.out.println((char) ('a' + j) + ") " + meerkeuzeAntwoorden[i][j]);
             }
-            System.out.print("Maak je keuze (bijv. abc): ");
+            System.out.print("Maak keuze (bijv. abc): ");
             String antwoord = scanner.nextLine();
             antwoorden.add(antwoord);
         }
