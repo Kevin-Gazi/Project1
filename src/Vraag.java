bcfffc4660ff89bdcbc3282bc3f52a52107d37e9
Jaouadjj <165282739+Jaouadjj@users.noreply.github.com>
2024-04-06 19:23:42 +0200

    class vraag

--- src/Vraag.java
+++ src/Vraag.java
@@ -1,22 +1,65 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Vraag {
-    private String vraag;
-    private ArrayList<String> vervolgVraag;
 
-    public Vraag (String vraag){
-        this.vraag = vraag;
-        this.vervolgVraag = new ArrayList<>();
-    }
-    public String getVraag() {
-        return vraag;
-    }
-    public void setVraag (String vraag) {
-        this.vraag = vraag;
-    }
-    public void voegVraagToe(String vervolgVraag) {
-        this.vervolgVraag.add(vervolgVraag);
-    }
+    Scanner scanner = new Scanner(System.in);
+
+    String[] openVragen = {
+            "wat is uw leeftijd?",
+            "Welke game trekt u van onze webshop het meest aan?",
+            "Zijn er andere soorten games die u in ons voorraad zou willen zien?"};
+
+    String[] openVragenMetVervolg = {
+            "Zou je onze website aanbevelen aan andere voor het achterlaten van reviews? (ja/nee)",
+            "Heb je ooit moeilijkheden ondervonden bij het vinden van een specifiek spel in de catalogus? (ja/nee)"};
+
+    // Meerkeuze vragen
+    String[] meerkeuzeVragen = {
+            "Welke soort games spreekt u het meest aan? (meerdere keuzes mogelijk)",
+            "Hoe tevreden ben je over de gebruiksvriendelijkheid voor het achterlaten van reviews? (meerdere keuzes mogelijk)"};
+    String[][] meerkeuzeOpties = {
+            {"Actie", "Avontuur", "Horror", "Strategie"},
+            {"Zeer tevreden", "Tevreden", "Neutraal", "Ontevreden", "Zeer ontevreden"}
+    };
+
+    List<String> antwoorden = new ArrayList<>();
 
 
+
+    public void VragenBeantwoorden() {
+
+        for (String vraag : openVragen) {
+            System.out.println(vraag);
+            String antwoord = scanner.nextLine();
+            antwoorden.add(antwoord);
+        }
+
+        for (String vraag : openVragenMetVervolg) {
+            System.out.println(vraag);
+            String antwoord = scanner.nextLine();
+            antwoorden.add(antwoord);
+
+            if (antwoord.equalsIgnoreCase("ja")) {
+                System.out.println("Waarom?");
+                antwoord = scanner.nextLine();
+                antwoorden.add(antwoord);
+            }
+        }
+
+        for (int i = 0; i < meerkeuzeVragen.length; i++) {
+            System.out.println(meerkeuzeVragen[i]);
+            for (int j = 0; j < meerkeuzeOpties[i].length; j++) {
+                System.out.println((char) ('a' + j) + ") " + meerkeuzeOpties[i][j]);
+            }
+            System.out.print("Maak je keuze (bijv. abc): ");
+            String antwoord = scanner.nextLine();
+            antwoorden.add(antwoord);
+        }
+    }
+
+    public List<String> getAntwoorden() {
+        return antwoorden;
+    }
 }
